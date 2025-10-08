1a074841bd8c507398050593adcab0af448179db
tensorg gl <gg.tensor@gmail.com>
2025-10-08 14:09:33 +0000

    make the logo 3x bigger and remove the text $TRAD

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -3,11 +3,8 @@ import Image from "next/image";
 
 export function Logo({ className }: { className?: string }) {
   return (
-    <a href="#" className={cn("flex items-center gap-2", className)}>
-      <Image src="https://i.postimg.cc/nzfPQwrY/download-1.png" alt="TradCoin Logo" width={32} height={32} />
-      <span className="font-headline text-2xl font-bold tracking-wider">
-        $TRAD
-      </span>
+    <a href="#" className={cn("flex items-center", className)}>
+      <Image src="https://i.postimg.cc/nzfPQwrY/download-1.png" alt="TradCoin Logo" width={96} height={96} />
     </a>
   );
 }
