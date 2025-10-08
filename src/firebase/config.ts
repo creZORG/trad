9fd15e9876e4b0d8a567ac1ab857a4725b021890
tensorg gl <gg.tensor@gmail.com>
2025-10-08 14:03:30 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9540981832-de09e",
+  "appId": "1:45537158473:web:b7c86eef5ff252088d2698",
+  "apiKey": "AIzaSyBB9hNBY79MEUL2V_2lttApXYulygwwtEc",
+  "authDomain": "studio-9540981832-de09e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "45537158473"
+};
